2ddab17e519c4738e57c32bfcf8972fd246f8cff
sairajeshd <>
2023-02-13 22:42:50 +0530

    TFcommit27

---
+++ 05-modules/ec2/data.tf
@@ -0,0 +1,5 @@
+data "aws_ami" "lab-image" {
+  most_recent      = true
+  name_regex       = "AnsibleImageSaiRajeshD"
+  owners           = ["self"]
+}
--- 05-modules/ec2/main.tf
+++ 05-modules/ec2/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "this" {
-  ami                     = "ami-06b80a951df965a53"
+  ami                     = "data.aws_ami.lab-image.image_id"
   instance_type           = "t2.micro"
   vpc_security_group_ids  = [var.sg]
 }
@@ -9,3 +9,12 @@ variable "sg" {}
 output "public-ip-address" {
   value = aws_instance.this.public_ip
 }
+
+
+# Patching of your EC2 & AMI has to be taken care as a part of the monthly maintenance.
+
+# How do we patch the AMI ???
+### Answer : Make an instance out of that AMI, update the server with latest pathces; Then , make AMI out of it with the same name and then deresigter the old AMI.
+#### Airlines-Proj-X-1-Feb ;  Airlines-Proj-X-2-Mar ;
+
+# PS : If you keep the provisioner inside the resource, if the provisioner fails for any reason, it assumes that the whole block is a failure and then it destroy and recreates in the next run.
