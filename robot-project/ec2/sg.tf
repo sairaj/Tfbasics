c53d87b1650be2641f3400b397f5eb3327452a26
sairajeshd <>
2023-02-13 23:56:37 +0530

    TFcommit36

--- robot-project/ec2/sg.tf
+++ robot-project/ec2/sg.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "allow_all" {
     description      = "SSH from public"
     from_port        = 0
     to_port          = 0
-    protocol         = "tcp"
+    protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]  # [] represents list
   }
 
