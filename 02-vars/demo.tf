58127e1ee120d8aa2330219cdad74158d601a22e
sairajeshd <>
2023-02-13 17:02:19 +0530

    TFcommit7

--- 02-vars/demo.tf
+++ 02-vars/demo.tf
@@ -3,3 +3,9 @@ variable "City" {} # This is how we declare an empty variable
 output "printing-city"{
     value = "value of the city is ${var.City}"
 }
+
+variable "State" {}
+
+output "print-state"{
+    value = "Name of the state is ${var.State}"
+}
