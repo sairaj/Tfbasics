8bc7e494869098ffc166f821dcc08ed9af207aa7
sairajeshd <>
2023-02-13 20:07:56 +0530

    TFcommit12

--- 03-ec2/ec2.tf
+++ 03-ec2/ec2.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "this" {
   ami                     = "ami-06b80a951df965a53"
-  instance_type           = "t3.micro"
+  instance_type           = "t2.micro"
 
   tags = {
     Name = "TF1"
