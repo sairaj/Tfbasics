29614c017824fad3462206373ec0e078fc073520
sairajeshd <>
2023-02-13 22:23:10 +0530

    TFcommit24

--- 05-modules/ec2/main.tf
+++ 05-modules/ec2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "this" {
   ami                     = "ami-06b80a951df965a53"
   instance_type           = "t2.micro"
-  vpc_security_group_ids  = [sg]
+  vpc_security_group_ids  = [var.sg]
 }
 
 variable "sg" {}
