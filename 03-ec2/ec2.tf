c608773e5708b7dc433c6f5ac4ff9ebf3be69a75
sairajeshd <>
2023-02-13 21:19:16 +0530

    TFcommit17

--- 03-ec2/ec2.tf
+++ 03-ec2/ec2.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_instance" "this" {
   ami                     = "ami-06b80a951df965a53"
   instance_type           = "t2.micro"
+  vpc_security_group_ids  = [aws_security_group.allow_ssh.id]
 
   tags = {
     Name = "Terraform-server"
