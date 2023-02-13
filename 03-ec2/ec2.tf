b596e03cf23dba6d561e76ecb6751c7b0055ef48
sairajeshd <>
2023-02-13 21:23:17 +0530

    TFcommit18

--- 03-ec2/ec2.tf
+++ 03-ec2/ec2.tf
@@ -18,9 +18,17 @@ resource "aws_security_group" "allow_ssh" {
   description = "Allow TLS inbound traffic"
 
   ingress {
-    description      = "SSH from SSH"
+    description      = "SSH from public"
     from_port        = 22
     to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]  # [] represents list
+  }
+
+    ingress {
+    description      = "HTTP from public"
+    from_port        = 80
+    to_port          = 80
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]  # [] represents list
   }
