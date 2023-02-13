ce7b6ad3bf483a1a3ba09684b76062dbda7f7682
sairajeshd <>
2023-02-13 16:43:41 +0530

    TFcommit3

--- 01-outputs/demo.tf
+++ 01-outputs/demo.tf
@@ -17,3 +17,8 @@ output "print-demo" {
 # Note : A variable can be accessed without ${} as well, only if its the only declared entry in the value part of the output. 
         // If you're using the variable in between the strings, you need to enclose them in the quotes and ${} . 
         // There is no concept of single quotes `'` in terraform
+
+# string, number , boolean data types are there in Terraform
+# Default variable type
+# List variable type
+# map variable type (python dictionary)
