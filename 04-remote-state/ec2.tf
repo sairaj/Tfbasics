b0b501e9285be7c7f67c8deaa7db81ef86fe9d5d
sairajeshd <>
2023-02-13 21:43:38 +0530

    TFcommit20

--- 04-remote-state/ec2.tf
+++ 04-remote-state/ec2.tf
@@ -12,39 +12,6 @@ resource "aws_instance" "this" {
   }
 }
 
-# Create security group
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow TLS inbound traffic"
-
-  ingress {
-    description      = "SSH from public"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]  # [] represents list
-  }
-
-    ingress {
-    description      = "HTTP from public"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]  # [] represents list
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "allow_ssh_tg"
-  }
-}
-
 # printing the public ip address of the machine
 output "public_dns_print" {
     value = aws_instance.this.public_dns
